85155a5970e70ced3e6f75d9c345f81cd26a15ca
Brandon VanCamp <brandon@overtimewebdev.com>
2019-09-06 17:19:26 -0500

    Create Home.js

---
+++ client/src/views/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h1>Project Home</h1>
+      {/* Link to List.js */}
+      <Link to={'./list'}>
+        <button variant="raised">
+            My List
+        </button>
+      </Link>
+    </div>
+    );
+  }
+}
+export default Home;
